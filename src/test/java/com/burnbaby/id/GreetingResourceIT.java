6809a59e492e76a8cd9b4f5660f495b9048eeb61
pramestyjaya <pramesty.jaya@gmail.com>
2023-05-05 10:59:55 +0700

    Initial commmit

---
+++ src/test/java/com/burnbaby/id/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.burnbaby.id;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
